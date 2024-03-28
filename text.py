eb2ecca2b4f6f8c7ff3e38cb6607223b70cca1b8
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-03-28 12:17:55 +0530

    testing the fucntion

--- text.py
+++ text.py
@@ -17,3 +17,28 @@ def extract_sentences(pdf_bytes):
     # remove empty sentences
     sentences = [s for s in sentences if s]
     return sentences
+
+def extract_sentences2(pdf_bytes):
+    doc = fitz.open("pdf", pdf_bytes)
+
+    sentences = []
+    
+    for page in doc:
+        text = page.get_text()
+        # split text into sentences using regular expression
+        sentences += re.split("(?<=[.!?])\s+", text)
+    
+    # remove extra whitespace and newlines
+    sentences = [s.strip().replace('\n', '') for s in sentences]
+    
+    # remove empty sentences
+    sentences = [s for s in sentences if s]
+    print(sentences)
+    print(type(sentences))
+    return sentences
+
+if __name__ == "__main__":
+    pdf_path = ''
+    with open(pdf_path, 'rb') as f:
+        file = f.read()
+    extract_sentences2(file)
