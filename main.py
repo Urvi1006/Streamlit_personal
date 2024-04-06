07225ea935346d7dca5055d1ff16c36bc64527f0
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-04-06 15:14:18 +0530

    main.py modified for collab

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from text import extract_sentences
 from LLM import LLM_Pipeline
 
-pdf_path = ''
+pdf_path = '/content/Streamlit_personal/data/Xometry Sheet Metal Design Guide 2020.pdf'
+csv_path = './output.csv'
 pipline = LLM_Pipeline()
 
 with open(pdf_path, 'rb') as f:
@@ -17,7 +18,7 @@ with open(pdf_path, 'rb') as f:
     print("RULES end   >>>>>>>>>>>>>>>>>>>>>>>>>>")
 
     # Convert to JSON from rules 
-    JSON = pipline.json_from_string(rules)
+    JSON = pipline.json_from_list(rules, csv_path)
     print("JSON Start >>>>>>>>>>>>>>>>>>>>>>>")
     print(JSON)
     print("JSON end   >>>>>>>>>>>>>>>>>>>>>>>>")
